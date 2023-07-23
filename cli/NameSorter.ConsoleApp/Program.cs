9a6ce9a0f400088a065983c5263de8cd03eef6f3
Dinindu Fernando <wdnfernando@gmail.com>
2023-07-23 18:14:46 +1000

    implement console application

---
+++ cli/NameSorter.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using NameSorter.Infrastructure;
+using NameSorter.Domain;
+using NameSorter.Application.Services;
+
+namespace NameSorter.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFile = "./unsorted-names-list.txt";
+
+        IFileReader fileReader = new FileReader();
+        IEnumerable<string> unsortedNames = fileReader.ReadLines(inputFile);
+
+        IParsingService parsingService = new ParsingService();
+        IEnumerable<Name> parsedNames = parsingService.ParseToNamesList(unsortedNames);
+
+        ISortingService sortingService = new SortingService();
+        IEnumerable<Name> sortedNames = sortingService.Sort(parsedNames);
+
+        IEnumerable<string> sortedNameStrings = parsingService.ParseToStringList(sortedNames);
+
+        string outputFile = "./sorted-names-list.txt";
+
+        IFileWriter fileWriter = new FileWriter();
+        fileWriter.WriteLines(outputFile, sortedNameStrings);
+
+        DisplaySortedNames(sortedNameStrings);
+
+    }
+
+    private static void DisplaySortedNames(IEnumerable<string> sortedNameStrings)
+    {
+        foreach (string name in sortedNameStrings)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
