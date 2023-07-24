87973381ad154e656fe6306a232de3d83f7eb4d5
Dinindu Fernando <wdnfernando@gmail.com>
2023-07-24 16:13:20 +1000

    File Reader unit tests

--- src/NameSorter.Infrastructure/FileReader.cs
+++ src/NameSorter.Infrastructure/FileReader.cs
@@ -5,6 +5,12 @@ public class FileReader : IFileReader
 {
     public IEnumerable<string> ReadLines(string filename)
     {
+
+        if (!File.Exists(filename))
+        {
+            throw new Exception($"file not found: '{filename}'");
+        }
+
         return File.ReadAllLines(filename);
     }
 }
---
+++ tests/NameSorter.Infrastructure.Tests/FileReader_UnitTests.cs
@@ -0,0 +1,21 @@
+using FakeItEasy;
+
+namespace NameSorter.Infrastructure.Tests;
+
+public class FileReader_UnitTests
+{
+    [Fact]
+    public void ReadLines_ShouldThrowAnErrorWhenFileIsNotFound()
+    {
+        // Arrange
+        IFileReader fileReader = new FileReader();
+        string unknownFile = "unknown-file.txt";
+
+        string expectedErrorMessage = "file not found: 'unknown-file.txt'";
+
+        // Act & Assert
+        Exception ex = Should.Throw<Exception>(() => fileReader.ReadLines(unknownFile));
+
+        ex.Message.ShouldBeEquivalentTo(expectedErrorMessage);
+    }
+}
